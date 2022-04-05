d67f73fe9c0ac0be310ebbfb6a9962f6f0bdceb3
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2022-04-05 15:16:18 +0700

    Update UI

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,27 +1,52 @@
-if (window.location.hash) {
-	var current = document.getElementsByClassName("active");
-	if (current.length > 0)
-		current[0].className = current[0].className.replace(" active", "");
-	var selected = window.location.hash.replace('#', '');
-	document.getElementsByClassName(selected)[0].className += " active";
-}
-
 let top_btn = document.getElementById("btn-back-to-top");
+let section = document.querySelectorAll("section");
+let sections = {};
+let i = 0;
+
+Array.prototype.forEach.call(section, function (e) {
+	sections[e.id] = e.offsetTop;
+});
+
+setActiveMenu();
+window.addEventListener('hashchange', setActiveMenu);
 
 window.onscroll = function () {
-	scrollFunction();
-};
-
-function scrollFunction() {
+	/* Su kien dung de len dau trang */
 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 		top_btn.style.display = "block";
 	} else {
 		top_btn.style.display = "none";
 	}
+	/* Su kien dung de danh dau menu khi cuon trang */
+	let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+	for (className in sections) {
+		// Neu o cuoi trang
+		if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+			window.location.hash = '#form';
+		}
+		// Neu o cac menu dau tien
+		else if (sections[className] <= scrollPosition + 100) {
+			removeActive();
+			document.getElementsByClassName(className)[0].className += " active";
+		}
+	}
+};
+
+function setActiveMenu() {
+	if (window.location.hash) {
+		removeActive();
+		let selected = window.location.hash.replace('#', '');
+		document.getElementsByClassName(selected)[0].className += " active";
+	}
 }
-top_btn.addEventListener("click", backToTop);
 
 function backToTop() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
+}
+
+function removeActive() {
+	let current = document.getElementsByClassName("active");
+	if (current.length > 0)
+		current[0].className = current[0].className.replace(" active", "");
 }
